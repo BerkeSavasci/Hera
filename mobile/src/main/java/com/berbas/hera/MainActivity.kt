e1a82939875767bbdf4a29c23b9b4fde7d768fcc
Berke <brk.svasci@gmail.com>
2024-04-24 20:14:26 +0200

    Created Bottom Navigation Bar added bottom navigation bar for the mobile app with goals, home and profile options. Implemented them as fragments and made the home fragment the default on launch

--- mobile/src/main/java/com/berbas/hera/MainActivity.kt
+++ mobile/src/main/java/com/berbas/hera/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.berbas.hera
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.berbas.hera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+        binding.bottomNavigationView.selectedItemId = R.id.home
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.goals -> replaceFragment(GoalFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                else -> {
+
+                }
+            }
+            true
         }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
