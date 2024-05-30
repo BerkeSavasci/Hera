637fee7c038fa4c37654b28f9b3b5fbe5bd59a21
Berke <brk.svasci@gmail.com>
2024-05-30 19:08:04 +0200

    forgot to push build.gradle.kts

--- heraConnectCommon/build.gradle.kts
+++ heraConnectCommon/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
 
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.room:room-runtime:2.6.1")
-    kapt ("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 
     implementation(libs.bson.kotlinx)
     implementation(libs.play.services.fitness)
